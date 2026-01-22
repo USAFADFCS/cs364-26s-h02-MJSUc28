776c76f29734921ce8ef11fda27d1cb97f5f5ec4
MJSUc28 <c28michael.ulery@afacademy.af.edu>
2026-01-21 19:21:52 -0700

    Initial Commit

--- cs364RegExH02.js
+++ cs364RegExH02.js
@@ -1,38 +1,38 @@
 /*
- * name:
+ * name: Michael Ulery
  * CS364 - H2
  * Documentation:
  *
  */
 
 //1: match the word "hello"
-const regex01 = 
+const regex01 = /hello/;
 
 //2: match phone numbers in the format (123) 456-7890
-const regex02 = 
+const regex02 = /\(\d{3}\)\s\d{3}-\d{4}/;
 
 //3: match valid email addresses
-const regex03 = 
+const regex03 = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
 
 //4: find all words that start with a capital letter
-const regex04 = 
+const regex04 = /[A-Z][a-z]+/;
 
 //5: match strings with any one of these characters: ; [, ], {, }, ^ 
-const regex05 = 
+const regex05 = /\b(\w*(\;|\[|\]|\{|\}|\^)+\w*)|((\;|\[|\]|\{|\}|\^)+\w*)/;
 
 //6: match dates in the format DD/MM/YYYY
-const regex06 = 
+const regex06 = /(\d{2}\/\d{2}\/\d{4})/;
 
 //7: find all words that are exactly 4 characters long
-const regex07 = 
+const regex07 = /\b\w{4}\b/;
 
 //8: to find sequences where any character is repeated three or more times 
-const regex08 = 
+const regex08 = /(\w)\1+/;
 
 //9: match URLs starting with http:// or https://
-const regex09 = 
+const regex09 = /\bhttps[A-Za-z0-9-.\\\:\/]*|\bhttp[A-Za-z0-9-.\\\:\/]*/;
 
 //10: Validate a Password
-const regex10 = 
+const regex10 = /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}/;
 
 
