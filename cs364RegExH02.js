c149c02b7d7d389508ddb79f70c186478b46a498
MJSUc28 <c28michael.ulery@afacademy.af.edu>
2026-01-22 18:56:23 -0700

    More Documentation

--- cs364RegExH02.js
+++ cs364RegExH02.js
@@ -1,7 +1,7 @@
 /*
  * name: Michael Ulery
  * CS364 - H2
- * Documentation: I sued the following site to vallidate my regex: https://regex101.com/. I recieved no other help or used any other resources.
+ * Documentation: I sued the following site to vallidate my regex: https://regex101.com/. I recieved no other help or used any other non-instructor resources.
  *
  */
 
